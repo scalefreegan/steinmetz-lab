1c57075a9443729a32e400e271e8f4d85f5aec2b
Aaron Brooks <aaron.brooks@embl.de>
2015-07-27 18:59:40 +0200

    another typo

--- shinyClustQTL/server.R
+++ shinyClustQTL/server.R
@@ -19,7 +19,7 @@ shinyServer(function(input, output) {
     data <- clust_qtls[[input$gene]]$data
 
     # find peaks
-    peaks = findQTLPeaks(clust_qtsl[[input$gene]]$qtl, markers_yeast, 
+    peaks = findQTLPeaks(clust_qtls[[input$gene]]$qtl, markers_yeast, 
                          pcutoff = .01, peak_sigma = 25, peak_threshold=1)
     
     # draw the qtl peak profile
