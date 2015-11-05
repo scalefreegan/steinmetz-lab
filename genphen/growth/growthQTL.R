80f0d3fb5efc888258fef680799f8e7118465db7
Aaron Brooks <aaron.brooks@embl.de>
2015-11-05 16:47:14 +0100

    corrected references

--- genphen/growth/growthQTL.R
+++ genphen/growth/growthQTL.R
@@ -82,7 +82,7 @@ cc = cc[,2:dim(cc)[2]]
 # LOG2 transform to make more normal
 cc = log2(cc)
 
-f = "/g/steinmetz/brooks/genphen/growth/qtls/ccQTL.rda"
+
 if (FALSE) {
 #if (!file.exists(f)) {
 	# group phenotype timepoints
@@ -160,3 +160,88 @@ if (FALSE) {
 # growth rate by regression using lm
 #
 #-------------------------------------------------------------------#
+# rate from linear model
+gr = filter(endometabolite, metabolite == "AKG", time_format == "absolute" ) %>%
+     select(., strain, replicate, time, cellconc) %>%
+     group_by(., strain) %>%
+     do({
+       x = as.numeric(.$time)
+       y = as.numeric(log2(.$cellconc))
+       o = lm(y~x)
+       return(data.frame(rate = as.numeric(o$coefficients[2])))
+       })
+gr = data.frame(rate = gr$rate, row.names = sapply(as.character(gr$strain),strainRename))
+
+f = "/g/steinmetz/brooks/genphen/growth/qtls/grQTL.rda"
+
+if (FALSE) {
+#if (!file.exists(f)) {
+	# group phenotype timepoints
+
+	cross_f = "/g/steinmetz/brooks/genphen/growth/cross_template.rda"
+	#cross_f_abs = "/g/steinmetz/brooks/genphen/metabolome/cross_template_abs.rda"
+	if (!file.exists(cross_f)) {
+		cross =	runQTL(
+					genotype = geno,
+					phenotype = gr,
+					marker_info = mrk,
+					return_cross = TRUE,
+					estimate.map = FALSE
+					)
+		save(cross, file = cross_f)
+	} else {
+		load(cross_f)
+	}
+
+	cross = calc.genoprob(cross, step = 0)
+	# last phenotype column is the "id" tag
+	pcols = seq(1, dim(cross$pheno)[2] - 1)
+ 	qtls = scanone(cross, method = "hk", pheno.col = pcols)
+	eff = geteffects(cross, pheno.col = pcols)
+	qtls_alt = scanoneF(cross, pheno.cols = pcols, method = "hk")
+	# calc permutation threshold
+	permout = scanoneF(cross, pheno.cols = pcols,
+	                    method = "hk", n.perm = 1000, n.cluster = 20, verbose = F)
+	# identify chrs with slod/lod above permute val
+	chrs = unique(c(
+		qtls_alt[qtls_alt[,"slod"]>=summary(permout)["5%","slod"],"chr"],
+		qtls_alt[qtls_alt[,"mlod"]>=summary(permout)["5%","mlod"],"chr"]
+		)
+	)
+	qtl_intervals = list()
+	if (length(chrs)>0) {
+		for (i in chrs) {
+			qtl_intervals[[as.character(i)]] = mrk[rownames(bayesint(qtls_alt, chr = str_pad(i, 2, pad = "0"), prob=0.95, lodcolumn=1))]
+		}
+	}
+	grQTL = list(qtls = qtls, eff = eff,
+				qtls_alt = qtls_alt, qtl_intervals = qtl_intervals,
+				permout = permout, pcols = pcols)
+	save(grQTL, file = f)
+	# plot stuff
+
+	# effect plots
+	pdf("/g/steinmetz/brooks/genphen/growth/plots/grQTL_effects.pdf")
+		plotlod(grQTL$qtls, grQTL$eff, grQTL$pcols, gap=25, ylab="Time")
+		mtext("grQTL", side = 3)
+	dev.off()
+
+	m_slod = max(grQTL$qtls_alt[,"slod"])
+	m_mlod = max(grQTL$qtls_alt[,"mlod"])
+
+	# lod plots
+	pdf("/g/steinmetz/brooks/genphen/growth/plots/grQTL_lods.pdf")
+			par(mfrow=c(2,1))
+			plot(grQTL$qtls_alt, ylim=c(0,m_slod), main=paste("grQTL SLOD"),bandcol="gray90")
+			abline(h=summary(grQTL$permout)["5%","slod"], col="red", lty=2)
+			abline(h=summary(grQTL$permout)["10%","slod"], col="blue", lty=3)
+			legend("topright", y.leg[i], c("5% FDR","10% FDR"), lty = c(2, 3), col = c("red","blue"))
+      plot(grQTL$qtls_alt, ylim=c(0,m_mlod), main=paste("grQTL MLOD"),bandcol="gray90")
+			abline(h=summary(grQTL$permout)["5%","mlod"], col="red", lty=2)
+			abline(h=summary(grQTL$permout)["10%","mlod"], col="blue", lty=3)
+			legend("topright", y.leg[i], c("5% FDR","10% FDR"), lty = c(2, 3), col = c("red","blue"))
+	dev.off()
+
+} else {
+	#load(f)
+}
