9774b8ef7655f6cf22efde77bc248b676b77fd0e
Aaron Brooks <aaron.brooks@embl.de>
2015-11-10 19:00:11 +0100

    run local

--- mQTL_explorer/processData.R
+++ mQTL_explorer/processData.R
@@ -13,7 +13,7 @@
 .status = "Development"
 .makeBW = FALSE
 .makeeQTL = FALSE
-.local = FALSE
+.local = TRUE
 # Import packages ---------------------------------------------------
 library(funqtl)
 library(GenomicRanges)
