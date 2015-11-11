a708619e15476c5b7e7b0d29a56a94b9df6ceb2a
Aaron Brooks <aaron.brooks@embl.de>
2015-11-11 14:40:49 +0100

    added global file

---
+++ mQTL_explorer/global.R
@@ -0,0 +1,60 @@
+# Import packages ---------------------------------------------------
+
+library(shiny)
+library(clustQTL)
+library(dplyr)
+library(reshape2)
+library(DT)
+library(parallel)
+library(GenomicRanges)
+library(ggplot2)
+library(funqtl)
+library(stringr)
+library("BSgenome.Scerevisiae.UCSC.sacCer3")
+library("TxDb.Scerevisiae.UCSC.sacCer3.sgdGene")
+library("org.Sc.sgd.db")
+
+# Global variables ---------------------------------------------------
+
+id2name = id2name(TxDb.Scerevisiae.UCSC.sacCer3.sgdGene)
+type = "mlod"
+
+# gene name map
+x = org.Sc.sgdGENENAME
+keys <- mappedkeys(x)
+# Convert to a list
+gname <- as.list(x[keys])
+
+# short description map
+x = org.Sc.sgdALIAS
+keys <- mappedkeys(x)
+# Convert to a list
+dname <- as.list(x[keys])
+
+# short description map
+x = org.Sc.sgdDESCRIPTION
+keys <- mappedkeys(x)
+# Convert to a list
+dname_long <- as.list(x[keys])
+
+# Web resources ---------------------------------------------------
+
+#addResourcePath('data', "/var/www2/html/mQTL/data")
+
+# Misc material ---------------------------------------------------
+
+# composite rda
+if (.local) {
+  DDIR = "/Users/brooks/Documents/steinmetz_local/genphen/metabolome"
+} else {
+  DDIR = "/g/steinmetz/brooks/genphen/metabolome/qtls"
+}
+
+
+f = file.path(DDIR,"mQTL.rda")
+if (!file.exists(f)) {
+  # load and process data
+  devtools::source_url("https://raw.githubusercontent.com/scalefreegan/steinmetz-lab/master/mQTL_explorer/processData.R")
+} else {
+  load(f)
+}
--- mQTL_explorer/server.R
+++ mQTL_explorer/server.R
@@ -1,4 +1,4 @@
-# This is the server logic for a Shiny web application.
+e# This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://www.rstudio.com/shiny/
@@ -23,67 +23,6 @@ if (system("hostname",intern=T) == "mac-steinmetz55.embl.de") {
   .local = TRUE
 } else {
   print(system("hostname"))
-}
-
-# Import packages ---------------------------------------------------
-
-library(shiny)
-library(clustQTL)
-library(dplyr)
-library(reshape2)
-library(DT)
-library(parallel)
-library(GenomicRanges)
-library(ggplot2)
-library(funqtl)
-library(stringr)
-library("BSgenome.Scerevisiae.UCSC.sacCer3")
-library("TxDb.Scerevisiae.UCSC.sacCer3.sgdGene")
-library("org.Sc.sgd.db")
-
-# Global variables ---------------------------------------------------
-
-id2name = id2name(TxDb.Scerevisiae.UCSC.sacCer3.sgdGene)
-type = "mlod"
-
-# gene name map
-x = org.Sc.sgdGENENAME
-keys <- mappedkeys(x)
-# Convert to a list
-gname <- as.list(x[keys])
-
-# short description map
-x = org.Sc.sgdALIAS
-keys <- mappedkeys(x)
-# Convert to a list
-dname <- as.list(x[keys])
-
-# short description map
-x = org.Sc.sgdDESCRIPTION
-keys <- mappedkeys(x)
-# Convert to a list
-dname_long <- as.list(x[keys])
-
-# Web resources ---------------------------------------------------
-
-#addResourcePath('data', "/var/www2/html/mQTL/data")
-
-# Misc material ---------------------------------------------------
-
-# composite rda
-if (.local) {
-  DDIR = "/Users/brooks/Documents/steinmetz_local/genphen/metabolome"
-} else {
-  DDIR = "/g/steinmetz/brooks/genphen/metabolome/qtls"
-}
-
-
-f = file.path(DDIR,"mQTL.rda")
-if (!file.exists(f)) {
-  # load and process data
-  devtools::source_url("https://raw.githubusercontent.com/scalefreegan/steinmetz-lab/master/mQTL_explorer/processData.R")
-} else {
-  load(f)
 }
 
 shinyServer(function(input, output, session) {
