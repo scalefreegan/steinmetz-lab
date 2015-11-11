89d8307589fa523387c3e7a3eeed6d0b54fd9795
Aaron Brooks <aaron.brooks@embl.de>
2015-11-11 14:57:03 +0100

    typo

--- mQTL_explorer/server.R
+++ mQTL_explorer/server.R
@@ -1,4 +1,4 @@
-e# This is the server logic for a Shiny web application.
+# This is the server logic for a Shiny web application.
 # You can find out more about building applications with Shiny here:
 #
 # http://www.rstudio.com/shiny/
