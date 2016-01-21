9c7a61ee2511c71a02d80ef53282f02e0f8c9528
Aaron Brooks <aaron.brooks@embl.de>
2016-01-21 15:28:19 +0100

    most fencet dirquant

--- general/folderQuant.py
+++ general/folderQuant.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
         for x,y in reader:
             name2email[x] = y
 
-    proc = subprocess.Popen(["find `pwd` -type f -exec ls -l -k --time-style=long-iso {} \; 2> /dev/null"], stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen(["find `pwd` -type f -exec ls -l -k --time-style=long-iso {} \;"], stdout=subprocess.PIPE, shell=True)
     (out, err) = proc.communicate()
     out2 = pd.DataFrame([s.split(None, 7) for s in out.splitlines()],
         columns = ["permissions","nlinks","owner","group","size","date","time","fname"])
@@ -63,35 +63,38 @@ if __name__ == "__main__":
                 owners_pass.append(i)
     owners_pass = ["brooks"] # for testing
     for i in owners_pass:
-        thisdf = out2.loc[out2.loc[:,"owner"]==i,:]
+        try:
+            thisdf = out2.loc[out2.loc[:,"owner"]==i,:]
 
-        # total usage GB
-        total = round(thisdf.loc[:,"size"].sum() / 1e6, 2)
-        median_other = round(out2.groupby("owner").agg({'size' : np.sum}).median()[0] / 1e6, 2)
+            # total usage GB
+            total = round(thisdf.loc[:,"size"].sum() / 1e6, 2)
+            median_other = round(out2.groupby("owner").agg({'size' : np.sum}).median()[0] / 1e6, 2)
 
-        # larger than 1 GB
-        thisdf_L = thisdf.loc[thisdf.loc[:,"size"]>=1e6,:]
-        thisdf_L = thisdf_L.sort_values("size",ascending=False)
-        thisL = (thisdf_L["size"]/1e6).map(str) + " " + thisdf_L["fname"]
-        lfiles = ("\n").join(j for j in thisL)
-        otherdf_L = out2.loc[out2.loc[:,"size"]>=1e6,:]
-        median_other_L = otherdf_L.groupby("owner").size().median()
+            # larger than 1 GB
+            thisdf_L = thisdf.loc[thisdf.loc[:,"size"]>=1e6,:]
+            thisdf_L = thisdf_L.sort_values("size",ascending=False)
+            thisL = (thisdf_L["size"]/1e6).map(str) + " " + thisdf_L["fname"]
+            lfiles = ("\n").join(j for j in thisL)
+            otherdf_L = out2.loc[out2.loc[:,"size"]>=1e6,:]
+            median_other_L = otherdf_L.groupby("owner").size().median()
 
-        # cum sum over time plot
-        out3 = calcDateCumSum(thisdf, startDate = "min", endMax = 3650, rangeFreq = "1D", corrF = 1e6)
-        myplt = out3.plot(x = "date", y = "usage", kind = "area", legend = False, title = "Tier-1 Data Usage: %s" % i)
-        myplt.set_xlabel("date")
-        myplt.set_ylabel("usage (GB)")
-        plt.savefig("/g/steinmetz/brooks/tier1-usage.png", dpi = 150)
+            # cum sum over time plot
+            out3 = calcDateCumSum(thisdf, startDate = "min", endMax = 3650, rangeFreq = "1D", corrF = 1e6)
+            myplt = out3.plot(x = "date", y = "usage", kind = "area", legend = False, title = "Tier-1 Data Usage: %s" % i)
+            myplt.set_xlabel("date")
+            myplt.set_ylabel("usage (GB)")
+            plt.savefig("/g/steinmetz/brooks/tier1-usage.png", dpi = 150)
 
-        # send mail
-        proc = subprocess.Popen(["df -h /g/steinmetz/"], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        usage = out.splitlines()[2].split()
-        statement = 'Hello from the Steinmetz Tier-1 Storage Robowizard,\n\nMy job is to alert you about data usage on the Steinmetz Tier-1 storage drive. Space on this centrally managed service is expensive and limited. Currently our Tier-1 storage drive is %s full! We are using %s of %s of purchased space.\n\nPlease carefully evaluate your usage. Delete unnessary files and archive old projects and files to Tier-2 at /g/tier2/steinmetz/\n\nSave space for others!!\n\nIf you need any assistance, you can send a mail to Aaron Brooks (aaron.brooks@embl.de)\n\nThanks for your help! You will recieve a biannual update about your usage.\n\nYour stats:\n\nA plot with your usage statistics over time is attached\n\nYou are using: %s GBs. The median usage for all other users is %s GBs.\n\nYou have %s files larger than 1 GB. Other users have %s on average.\n\nYour large files (>= 1GB) include: \n\nSize(GB) File\n%s'
+            # send mail
+            proc = subprocess.Popen(["df -h /g/steinmetz/"], stdout=subprocess.PIPE, shell=True)
+            (out, err) = proc.communicate()
+            usage = out.splitlines()[2].split()
+            statement = 'Dear %s,\n\nHello from the Steinmetz Tier-1 Storage Robowizard!\n\nMy job is to alert you about data usage on the Steinmetz Tier-1 storage drive. Space on this centrally managed service is expensive and limited. Currently our Tier-1 storage drive is %s full! We are using %s of %s of purchased space.\n\nPlease carefully evaluate your usage. Delete unnessary files and archive old projects and files to Tier-2 at /g/tier2/steinmetz/\n\nSave space for others!!\n\nIf you need any assistance, you can send a mail to Aaron Brooks (aaron.brooks@embl.de)\n\nThanks for your help! You will recieve a biannual update about your usage.\n\nYour stats:\n\nA plot with your usage statistics over time is attached\n\nYou are using: %s GBs. The median usage for all other users is %s GBs.\n\nYou have %s files larger than 1 GB. Other users have %s on average.\n\nYour large files (>= 1GB) include: \n\nSize(GB) File\n%s'
 
-        # send mail
-        msend3 = 'echo "%s" | mailx -v -s "Tier-1 Usage Report" -a "/g/steinmetz/brooks/tier1-usage.png" %s'
-        m = statement % (usage[3], usage[1], usage[0], total, median_other, thisdf_L.size, median_other_L, lfiles)
-        proc = subprocess.Popen([msend3 % (m,name2email[i])], stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
+            # send mail
+            msend3 = 'echo "%s" | mailx -v -s "Tier-1 Usage Report" -a "/g/steinmetz/brooks/tier1-usage.png" %s'
+            m = statement % (i, usage[3], usage[1], usage[0], total, median_other, thisdf_L.size, median_other_L, lfiles)
+            proc = subprocess.Popen([msend3 % (m,name2email[i])], stdout=subprocess.PIPE, shell=True)
+            (out, err) = proc.communicate()
+        except:
+            pass
---
+++ general/startIpythonNotebook.py
@@ -0,0 +1,19 @@
+# set anaconda to py27 env
+# source activate py27
+
+import sys
+from IPython.terminal.ipapp import launch_new_instance
+from IPython.lib import passwd
+from socket import gethostname
+import warnings
+
+warnings.filterwarnings("ignore", module = "zmq.*")
+sys.argv.append("notebook")
+sys.argv.append("--IPKernelApp.pylab='inline'")
+sys.argv.append("--NotebookApp.ip=" + gethostname())
+sys.argv.append("--NotebookApp.open_browser=False")
+sys.argv.append("--NotebookApp.password=" + passwd())
+sys.argv.append("--NotebookApp.port=1984")
+sys.argv.append("--notebook-dir=u'/g/steinmetz/brooks/'")
+
+launch_new_instance()
