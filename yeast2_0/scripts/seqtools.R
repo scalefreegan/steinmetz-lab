b1aa20a4695f91dacf7873683598ba476901956f
Aaron Brooks <aaron.brooks@embl.de>
2018-07-04 10:56:25 +0200

    explicit stringr call

--- yeast2_0/scripts/seqtools.R
+++ yeast2_0/scripts/seqtools.R
@@ -38,7 +38,7 @@ roman2arabic <- function(x, Mitochondrial = "XVII") {
   x.copy[x.copy=="Mito"] = Mitochondrial
   x.copy[x.copy=="M"] = Mitochondrial
   x.copy[x.copy=="mt"] = Mitochondrial
-  x.copy = str_pad(as.integer(as.roman(x.copy)), 2, pad = "0")
+  x.copy = stringr::str_pad(as.integer(as.roman(x.copy)), 2, pad = "0")
   if (sum(is.na(x.copy))>0) {
     cat("Substituting chr names that were not Roman numerals to original names")
     toreplace = which(is.na(x.copy))
